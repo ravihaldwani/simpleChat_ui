f71e3adea7a8aef6de83cc8b4b60ae174f2ce049
Ankit Sharma <sankit72@gmail.com>
2022-09-03 11:24:31 +0530

    added sse service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { SseService } from './core/services/sse.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'chatApp';
+
+  constructor(private sseService: SseService) {}
+
+  ngOnInit() {
+    this.sseService
+      .getServerSentEvent('http://localhost:8080/chat/id/1')
+      .subscribe(console.log);
+  }
 }
